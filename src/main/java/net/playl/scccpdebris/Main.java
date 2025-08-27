bc9fd0e276dd77dc99c57f94c29110bb828009b2
D3it7i <36127740+D3it7i@users.noreply.github.com>
2025-08-28 02:24:39 +0800

    fix SecurityPacket register

--- src/main/java/net/playl/scccpdebris/Main.java
+++ src/main/java/net/playl/scccpdebris/Main.java
@@ -3,6 +3,7 @@ package net.playl.scccpdebris;
 import com.github.retrooper.packetevents.PacketEvents;
 import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
 import net.playl.scccpdebris.Listen.SecurityPacket;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -17,6 +18,8 @@ public class Main extends JavaPlugin {
         //We register before calling PacketEvents#init, because that method might already call some events.
         PacketEvents.getAPI().getEventManager().registerListener(new SecurityPacket(this));
         PacketEvents.getAPI().init();
+
+        Bukkit.getServer().getPluginManager().registerEvents(new SecurityPacket(this), this);
     }
     
     public void onDisable() {
